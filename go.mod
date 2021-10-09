52e6f2914630405ab32faf76be3874953f758976
Phuong Nguyen <phuongnguyen@Phuongs-iMac.local>
2021-10-10 00:40:16 +0700

    update

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/phuongnm1593/caddy-rta-es
+
+go 1.17
---
+++ rtaes.go
@@ -0,0 +1,111 @@
+package caddyrtaes
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+)
+
+func init() {
+	caddy.RegisterModule(Middleware{})
+	httpcaddyfile.RegisterHandlerDirective("rtaes", parseCaddyfile)
+}
+
+// Middleware implements an HTTP handler that writes the
+// visitor's IP address to a file or stream.
+type Middleware struct {
+	// The file or stream to write to. Can be "stdout"
+	// or "stderr".
+	// Output string `json:"output,omitempty"`
+
+	// w io.Writer
+}
+
+// CaddyModule returns the Caddy module information.
+func (Middleware) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.rtaes",
+		New: func() caddy.Module { return new(Middleware) },
+	}
+}
+
+// Provision implements caddy.Provisioner.
+func (m *Middleware) Provision(ctx caddy.Context) error {
+	m.logger = ctx.Logger(rewr)
+	// switch m.Output {
+	// case "stdout":
+	// 	m.w = os.Stdout
+	// case "stderr":
+	// 	m.w = os.Stderr
+	// default:
+	// 	return fmt.Errorf("an output stream is required")
+	// }
+	return nil
+}
+
+// Validate implements caddy.Validator.
+func (m *Middleware) Validate() error {
+	// if m.w == nil {
+	// 	return fmt.Errorf("no writer")
+	// }
+	return nil
+}
+
+// ServeHTTP implements caddyhttp.MiddlewareHandler.
+func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	logger := m.logger.With(
+		zap.Object("request", caddyhttp.LoggableHTTPRequest{Request: r}),
+	)
+
+	changed := m.handle(r, repl, logger)
+
+	if changed {
+		logger.Debug("rewrote request",
+			zap.String("uri", r.RequestURI),
+		)
+	}
+
+	return next.ServeHTTP(w, r)
+}
+
+// rewrite performs the rewrites on r using repl, which should
+// have been obtained from r, but is passed in for efficiency.
+// It returns true if any changes were made to r.
+func (m Middleware) handle(r *http.Request, repl *caddy.Replacer, logger *zap.Logger) bool {
+	oldURI := r.RequestURI
+
+	r.URL.RawQuery = "lala=sdsd"
+
+	// update the encoded copy of the URI
+	r.RequestURI = r.URL.RequestURI()
+
+	// return true if anything changed
+	return r.RequestURI != oldURI
+}
+
+// UnmarshalCaddyfile implements caddyfile.Unmarshaler.
+func (m *Middleware) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	
+	return nil
+}
+
+// parseCaddyfile unmarshals tokens from h into a new Middleware.
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var m Middleware
+	err := m.UnmarshalCaddyfile(h.Dispenser)
+	return m, err
+}
+
+// Interface guards
+var (
+	_ caddy.Provisioner           = (*Middleware)(nil)
+	_ caddy.Validator             = (*Middleware)(nil)
+	_ caddyhttp.MiddlewareHandler = (*Middleware)(nil)
+	_ caddyfile.Unmarshaler       = (*Middleware)(nil)
+)
