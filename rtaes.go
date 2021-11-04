787458ed1c14fb26577c0415476f88e4e8031d29
Phuong Nguyen <phuongnguyen@rta.vn>
2021-11-04 12:57:13 +0700

    reset

--- rtaes.go
+++ rtaes.go
@@ -4,9 +4,9 @@ import (
 	
 	"net/http"
 	// "strings"
-	"io"
+	// "io"
 	// "io/ioutil"
-	"os"
+	// "os"
 	// "encoding/json"
 
 	"github.com/caddyserver/caddy/v2"
@@ -15,9 +15,9 @@ import (
 	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
 	"go.uber.org/zap"
 
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
+	// "crypto/aes"
+	// "crypto/cipher"
+	// "crypto/rand"
 	// "fmt"
 )
 
@@ -73,47 +73,17 @@ func (m Middleware) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddy
 		zap.Object("request", caddyhttp.LoggableHTTPRequest{Request: r}),
 	)
 
-	// changed := m.handle(r, logger)
+	changed := m.handle(r, logger)
 
-	// if changed {
-	// 	logger.Debug("rewrote request",
-	// 		zap.String("uri", r.RequestURI),
-	// 	)
+	if changed {
+		logger.Debug("rewrote request",
+			zap.String("uri", r.RequestURI),
+		)
 	}
 
 	return next.ServeHTTP(w, r)
 }
 
-func encrypt(text []byte) []byte {
-	key := []byte(os.Getenv("CIPHER_KEY"))
-	c, _ := aes.NewCipher(key)
-	gcm, _ := cipher.NewGCM(c)
-
-	nonce := make([]byte, gcm.NonceSize())
-	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
-		panic(err.Error())
-	}
-
-	result := gcm.Seal(nonce, nonce, text, nil)
-
-	return result
-}
-
-func decrypt(ciphertext []byte) string {
-	key := []byte(os.Getenv("CIPHER_KEY"))
-	c, _ := aes.NewCipher(key)
-	gcm, _ := cipher.NewGCM(c)
-
-	nonceSize := gcm.NonceSize()
-	if len(ciphertext) < nonceSize {
-		panic("ciphertext size is less than nonceSize")
-	}
-
-	nonce, ciphertext := ciphertext[:nonceSize], ciphertext[nonceSize:]
-	plaintext, _ := gcm.Open(nil, nonce, ciphertext, nil)
-
-	return string(plaintext)
-}
 
 // rewrite performs the rewrites on r using repl, which should
 // have been obtained from r, but is passed in for efficiency.
